05e9e17e5800a6cd81dd5c84e9b96e91641e44e1
Khuslen Ganzorig <khusy.gan@gmail.com>
2023-06-02 10:15:05 +0100

    passing POST 'Voyage' album

--- music-app/spec/integration/application_spec.rb
+++ music-app/spec/integration/application_spec.rb
@@ -31,4 +31,19 @@ describe Application do
         expect(response.body).to include('OK Computer')
       end 
     end 
+
+    context 'POST /albums' do
+      it 'should create a new album' do
+        response = post('/albums',
+        title: 'Voyage',
+        release_year: '2022',
+        artist_id: '2')
+
+        expect(response.status).to eq(200)
+        expect(response.body).to eq('')
+
+        response = get('/albums')
+        expect(response.body).to include('Voyage')
+      end
+    end 
 end 
